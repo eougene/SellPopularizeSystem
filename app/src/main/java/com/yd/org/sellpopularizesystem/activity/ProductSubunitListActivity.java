87f2a855a1e5d704c7b429ae3754b4a00224b9c0
eougene <1365423747@qq.com>
2017-04-28 18:19:50 +0800

    修改

--- app/src/main/java/com/yd/org/sellpopularizesystem/activity/ProductSubunitListActivity.java
+++ app/src/main/java/com/yd/org/sellpopularizesystem/activity/ProductSubunitListActivity.java
@@ -35,14 +35,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductSubunitListActivity extends BaseActivity {
-    private Button btViewDetail,btRemain,btCancel;
+    private Button btViewDetail, btRemain, btCancel;
     private int pos;
     private RelativeLayout rlPop;
-    private TextView tvLocation,tvHouseType,tvArea,tvPrice,tvSelect;
+    private TextView tvLocation, tvHouseType, tvArea, tvPrice, tvSelect;
     private ListView lvHouseDetail;
     private ImageView ivSearch;
     private View mView;
-    private List<ProductChildBean> data=new ArrayList<ProductChildBean>();
+    private List<ProductChildBean> data = new ArrayList<ProductChildBean>();
     private CommonAdapter adapter;
     private Object bean;
     private ProSubUnitClassifyBean childBean;
@@ -50,8 +50,10 @@ public class ProductSubunitListActivity extends BaseActivity {
     private CustomePopuWindow mCustomePopuWindow;
     private String bedRoomNum;
     private String product_id;
-    private String string;
-    private String page="1";
+    private String string="";
+    private String page = "1";
+    //从产品中点击预订跳转标志
+    private String flag = "";
 
     @Override
     protected int setContentView() {
@@ -62,74 +64,82 @@ public class ProductSubunitListActivity extends BaseActivity {
     @Override
     public void initView() {
         getViews();
-        Bundle bundle=getIntent().getExtras();
-        bean= bundle.get("bean");
-        if (bean instanceof List){//点击查看所有传递过来的集合对象
+        Bundle bundle = getIntent().getExtras();
+        bean = bundle.get("bean");
+        product_id = (String) bundle.get("productId");
+        flag = (String) bundle.get("pidatopsla");
+        if (flag.equals("pidatopsla")) {
+            string = (String) bundle.get("title");
+            setTitle(string);
+            getListData();
+        }
+        if (bean != null) {
+            if (bean instanceof List) {//点击查看所有传递过来的集合对象
             /*data.clear();
             data.addAll((List)bean);
             Log.e("TAG", "initView: "+"集合");*/
-            product_id = (String) bundle.get("productId");
-            String proName=bundle.getString("productName");
-            setTitle(proName);
-            getListData();
-        }else{//点击单个item传递过来的对象
-            childBean= (ProSubUnitClassifyBean) bean;
+                String proName = bundle.getString("productName");
+                setTitle(proName);
+                getListData();
+            } else {//点击单个item传递过来的对象
+                childBean = (ProSubUnitClassifyBean) bean;
             /*data.clear();
             data.add(childBean);*/
-            bedRoomNum=childBean.getBedroom();
-            product_id = (String) bundle.get("productId");
-            string = bundle.getString("productName");
-            setTitle(string);
-            getListData();
+                bedRoomNum = childBean.getBedroom();
+                product_id = (String) bundle.get("productId");
+                string = bundle.getString("productName");
+                setTitle(string);
+                getListData();
+            }
         }
     }
 
     private void getViews() {
-        tvSelect=getViewById(R.id.tvSelect);
+        tvSelect = getViewById(R.id.tvSelect);
         tvSelect.setVisibility(View.VISIBLE);
-        ivSearch=getViewById(R.id.rightSearchLinearLayout);
+        ivSearch = getViewById(R.id.rightSearchLinearLayout);
         ivSearch.setVisibility(View.GONE);
         tvSelect.setOnClickListener(mOnClickListener);
         //tvRightDes.setBackgroundColor(Color.parseColor("#e14143"));
         //tvRightDes.setBackground(ContextCompat.getDrawable(this,R.drawable.button_bac));
-        tvLocation=getViewById(R.id.tvLocation);
-        tvHouseType=getViewById(R.id.tvHouseType);
-        tvArea=getViewById(R.id.tvArea);
-        tvPrice=getViewById(R.id.tvPrice);
-        lvHouseDetail=getViewById(R.id.lvHouseDetail);
+        tvLocation = getViewById(R.id.tvLocation);
+        tvHouseType = getViewById(R.id.tvHouseType);
+        tvArea = getViewById(R.id.tvArea);
+        tvPrice = getViewById(R.id.tvPrice);
+        lvHouseDetail = getViewById(R.id.lvHouseDetail);
 
-        mCustomePopuWindow=new CustomePopuWindow(ProductSubunitListActivity.this,mOnClickListener);
-        mView=mCustomePopuWindow.getContentView();
-        rlPop= (RelativeLayout) mView.findViewById(R.id.rlPop);
+        mCustomePopuWindow = new CustomePopuWindow(ProductSubunitListActivity.this, mOnClickListener);
+        mView = mCustomePopuWindow.getContentView();
+        rlPop = (RelativeLayout) mView.findViewById(R.id.rlPop);
         rlPop.setOnClickListener(mOnClickListener);
-        btViewDetail= (Button) mView.findViewById(R.id.btViewDetail);
+        btViewDetail = (Button) mView.findViewById(R.id.btViewDetail);
         btRemain = (Button) mView.findViewById(R.id.btRemain);
-        btCancel= (Button) mView.findViewById(R.id.btCancel);
+        btCancel = (Button) mView.findViewById(R.id.btCancel);
     }
 
     private void getListData() {
         showDialog();
-        FinalHttp fh=new FinalHttp();
-        AjaxParams ajaxParams=new AjaxParams();
+        FinalHttp fh = new FinalHttp();
+        AjaxParams ajaxParams = new AjaxParams();
         ajaxParams.put("user_id", SharedPreferencesHelps.getUserID());
-        ajaxParams.put("product_id",product_id);
-        ajaxParams.put("page",page);
-        ajaxParams.put("number",10+"");
-        ajaxParams.put("provice","");
-        ajaxParams.put("city","");
-        ajaxParams.put("town","");
-        ajaxParams.put("address","");
-        ajaxParams.put("bedroom",bedRoomNum);
-        ajaxParams.put("bathroom","");
-        ajaxParams.put("car_space","");
-        ajaxParams.put("has_study","");
-        ajaxParams.put("ensuite","");
-        ajaxParams.put("level","");
-        ajaxParams.put("price","0~100000000");
-        ajaxParams.put("internal","0~100000000");
-        ajaxParams.put("external","0~100000000");
-        ajaxParams.put("building_area","0~100000000");
-        ajaxParams.put("is_lock","");
+        ajaxParams.put("product_id", product_id);
+        ajaxParams.put("page", page);
+        ajaxParams.put("number", 10 + "");
+        ajaxParams.put("provice", "");
+        ajaxParams.put("city", "");
+        ajaxParams.put("town", "");
+        ajaxParams.put("address", "");
+        ajaxParams.put("bedroom", bedRoomNum == null ? "" : bedRoomNum);
+        ajaxParams.put("bathroom", "");
+        ajaxParams.put("car_space", "");
+        ajaxParams.put("has_study", "");
+        ajaxParams.put("ensuite", "");
+        ajaxParams.put("level", "");
+        ajaxParams.put("price", "0~100000000");
+        ajaxParams.put("internal", "0~100000000");
+        ajaxParams.put("external", "0~100000000");
+        ajaxParams.put("building_area", "0~100000000");
+        ajaxParams.put("is_lock", "");
         fh.get(Contants.PRODUCT_SUBUNIT_LIST, ajaxParams, new AjaxCallBack<String>() {
             @Override
             public void onSuccess(String s) {
@@ -147,103 +157,104 @@ public class ProductSubunitListActivity extends BaseActivity {
     }
 
     private void parseJson(String s) {
-        Gson gson=new Gson();
-        ProductSubunitListBean pslb=gson.fromJson(s,ProductSubunitListBean.class);
-        data=pslb.getResult();
+        Gson gson = new Gson();
+        ProductSubunitListBean pslb = gson.fromJson(s, ProductSubunitListBean.class);
+        data = pslb.getResult();
         setAdapter();
     }
+
     private void setAdapter() {
-        adapter = new CommonAdapter<ProductChildBean>(this,data,R.layout.productdetaill_activity_listview_item) {
+        adapter = new CommonAdapter<ProductChildBean>(this, data, R.layout.productdetaill_activity_listview_item) {
             @Override
             public void convert(ViewHolder holder, ProductChildBean item) {
-                Log.e("tag", "convert: "+item.getIs_lock());
-                if(item.getIs_lock()==0){
-                    holder.setImageResource(R.id.ivHouse,R.mipmap.greenhome);
-                }else if (item.getIs_lock()==1){
-                    holder.setImageResource(R.id.ivHouse,R.mipmap.redhome);
-                }else{
-                    holder.setImageResource(R.id.ivHouse,R.mipmap.yellowhome);
+                Log.e("tag", "convert: " + item.getIs_lock());
+                if (item.getIs_lock() == 0) {
+                    holder.setImageResource(R.id.ivHouse, R.mipmap.greenhome);
+                } else if (item.getIs_lock() == 1) {
+                    holder.setImageResource(R.id.ivHouse, R.mipmap.redhome);
+                } else {
+                    holder.setImageResource(R.id.ivHouse, R.mipmap.yellowhome);
                 }
-                if (!item.getThumb().equals("")){
+                if (!item.getThumb().equals("")) {
                     //holder.setImageByUrl(R.id.ivHousePhoto,item.getThumb(),mOnClickListener);
                     holder.getView(R.id.tvNoPhoto).setVisibility(View.GONE);
                     holder.getView(R.id.ivHousePic).setVisibility(View.VISIBLE);
                     holder.getView(R.id.ivHousePic).setOnClickListener(mOnClickListener);
-                }else{
+                } else {
                     holder.getView(R.id.tvNoPhoto).setVisibility(View.VISIBLE);
                     holder.getView(R.id.ivHousePic).setVisibility(View.GONE);
                 }
-                holder.setText(R.id.tvDetailLoc,item.getProduct_childs_unit_number());
-                holder.setText(R.id.tvBedRoom,item.getBedroom());
-                holder.setText(R.id.tvBathroom,item.getBathroom());
-                holder.setText(R.id.tvCarSquare,item.getCar_space());
-                holder.setText(R.id.tvHouseSquare,item.getBuilding_area());
-                holder.setText(R.id.tvDetailPrice,item.getPrice());
+                holder.setText(R.id.tvDetailLoc, item.getProduct_childs_unit_number());
+                holder.setText(R.id.tvBedRoom, item.getBedroom());
+                holder.setText(R.id.tvBathroom, item.getBathroom());
+                holder.setText(R.id.tvCarSquare, item.getCar_space());
+                holder.setText(R.id.tvHouseSquare, item.getBuilding_area());
+                holder.setText(R.id.tvDetailPrice, item.getPrice());
             }
         };
         lvHouseDetail.setAdapter(adapter);
     }
 
-    View.OnClickListener mOnClickListener=new View.OnClickListener() {
-
+    View.OnClickListener mOnClickListener = new View.OnClickListener() {
         private Bundle bund;
 
         @Override
         public void onClick(View v) {
-            switch (v.getId()){
+            switch (v.getId()) {
                 case R.id.rightTitle:
                     break;
                 case R.id.ivHousePic:
                     break;
                 case R.id.rlPop:
-                    if (mCustomePopuWindow!=null){
+                    if (mCustomePopuWindow != null) {
                         mCustomePopuWindow.dismiss();
                     }
                     break;
                 case R.id.btViewDetail:
-                    if (bund==null){
+                    if (bund == null) {
                         bund = new Bundle();
-                        bund.putSerializable("item", (Serializable) data.get(pos));
+                        bund.putSerializable("item", data.get(pos));
                     }
-                        ActivitySkip.forward(ProductSubunitListActivity.this,ProductSubItemDetailActivity.class, bund);
+                    ActivitySkip.forward(ProductSubunitListActivity.this, ProductSubItemDetailActivity.class, bund);
                     mCustomePopuWindow.dismiss();
                     break;
                 case R.id.btRemain:
-                    if (bund==null){
+                    if (bund == null) {
                         bund = new Bundle();
-                        bund.putSerializable("item", (Serializable) data.get(pos));
+                        bund.putSerializable("item", data.get(pos));
                     }
-                        ActivitySkip.forward(ProductSubunitListActivity.this,ReserveActivity.class,bund);
+                    ActivitySkip.forward(ProductSubunitListActivity.this, ReserveActivity.class, bund);
                     mCustomePopuWindow.dismiss();
                     break;
                 case R.id.btCancel:
-                    if (mCustomePopuWindow!=null){
+                    if (mCustomePopuWindow != null) {
                         mCustomePopuWindow.dismiss();
                     }
                     break;
             }
         }
     };
+
     @Override
     public void setListener() {
         btViewDetail.setOnClickListener(mOnClickListener);
         btRemain.setOnClickListener(mOnClickListener);
         btCancel.setOnClickListener(mOnClickListener);
-            lvHouseDetail.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        mCustomePopuWindow.showAtLocation(ProductSubunitListActivity.this.findViewById(R.id.llProdet), Gravity.BOTTOM, 0, 0);
-                    pos=position;
-                    if (data.get(pos).getIs_lock()==1){
-                        btRemain.setVisibility(View.GONE);
-                    }else if (data.get(pos).getIs_lock()==0){
-                        btRemain.setVisibility(View.VISIBLE);
-                    }
+        lvHouseDetail.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mCustomePopuWindow.showAtLocation(ProductSubunitListActivity.this.findViewById(R.id.llProdet), Gravity.BOTTOM, 0, 0);
+                pos = position;
+                if (data.get(pos).getIs_lock() == 1) {
+                    btRemain.setVisibility(View.GONE);
+                } else if (data.get(pos).getIs_lock() == 0) {
+                    btRemain.setVisibility(View.VISIBLE);
                 }
-            });
+            }
+        });
     }
 
-    class CustomePopuWindow extends CommonPopuWindow{
+    class CustomePopuWindow extends CommonPopuWindow {
 
         public CustomePopuWindow(Activity context, View.OnClickListener itemsOnClick) {
             super(context, itemsOnClick);
