ddb6d6d0afc9b53165249ba00b1560c74fa87b91
eougene <1365423747@qq.com>
2017-05-27 16:31:08 +0800

    变动

--- app/src/main/java/com/yd/org/sellpopularizesystem/activity/InvestigationActivity.java
+++ app/src/main/java/com/yd/org/sellpopularizesystem/activity/InvestigationActivity.java
@@ -1,11 +1,16 @@
 package com.yd.org.sellpopularizesystem.activity;
 
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bigkoo.pickerview.OptionsPickerView;
+import com.bigkoo.pickerview.listener.CustomListener;
 import com.squareup.picasso.Picasso;
 import com.yd.org.sellpopularizesystem.R;
 import com.yd.org.sellpopularizesystem.application.BaseApplication;
@@ -13,10 +18,15 @@ import com.yd.org.sellpopularizesystem.application.Contants;
 import com.yd.org.sellpopularizesystem.javaBean.CustomBean;
 import com.yd.org.sellpopularizesystem.myView.CircleImageView;
 import com.yd.org.sellpopularizesystem.utils.ActivitySkip;
+import com.yd.org.sellpopularizesystem.utils.MyUtils;
 import com.yd.org.sellpopularizesystem.utils.SharedPreferencesHelps;
 
 import net.tsz.afinal.FinalHttp;
 import net.tsz.afinal.http.AjaxParams;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 /**
  * 开始调研页面
@@ -27,6 +37,11 @@ public class InvestigationActivity extends BaseActivity {
     private EditText etContact, etAddress;
     private CircleImageView ivCustomePhoto;
     private CustomBean.ResultBean resultBean;
+    private OptionsPickerView pvCustomTime;
+    private List<String> weeks=new ArrayList<>();
+    private List<String> hours=new ArrayList<>();
+    private List<String> minutes=new ArrayList<>();
+    private Calendar cal;
 
     @Override
     protected int setContentView() {
@@ -73,7 +88,66 @@ public class InvestigationActivity extends BaseActivity {
         } else {
             ivCustomePhoto.setImageResource(R.mipmap.settingbt);
         }
+        //初始化自定义选择器的数据
+        MyUtils.getInstance().getOptionData(InvestigationActivity.this,weeks,hours,minutes);
+        //初始化自定义选择器
+        initOptionPicker();
+    }
 
+    private void initOptionPicker() {
+        cal = Calendar.getInstance();
+        OptionsPickerView.Builder builder = new OptionsPickerView.Builder(this, new OptionsPickerView.OnOptionsSelectListener() {
+            @Override
+            public void onOptionsSelect(int options1, int options2, int options3, View v) {
+                setText(cal, options1, options2, options3);
+            }
+        }).setTitleText(getString(R.string.uploadetime)).setTitleColor(R.color.black)
+                .setCyclic(true, true, true)/*.setOutSideCancelable(false)*/
+                .setSelectOptions(weeks.indexOf("今天"), hours.indexOf(String.format("%02d", cal.get(Calendar.HOUR_OF_DAY))), minutes.indexOf(String.format("%02d", cal.get(Calendar.MINUTE))))
+                .isDialog(true);
+
+        builder.setLayoutRes(R.layout.pickerview_custom_time, new CustomListener() {
+            @Override
+            public void customLayout(View v) {
+                TextView tvFinish = (TextView) v.findViewById(R.id.tv_finish);
+                TextView tvTitle = (TextView) v.findViewById(R.id.tvTitle);
+                ImageView ivUp = (ImageView) v.findViewById(R.id.ivUp);
+                ImageView ivDown = (ImageView) v.findViewById(R.id.ivDown);
+                tvTitle.setText(getString(R.string.uploadetime));
+                ivUp.setVisibility(View.GONE);
+                ivDown.setVisibility(View.GONE);
+                tvFinish.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        pvCustomTime.returnData();
+                    }
+                });
+
+            }
+        });
+        pvCustomTime = new OptionsPickerView(builder);
+        View view = LayoutInflater.from(this).inflate(R.layout.pickerview_custom_time, null);
+        LinearLayout llOptionPicker = (LinearLayout) view.findViewById(R.id.optionspicker);
+        pvCustomTime.setNPicker(weeks, hours, minutes);
+    }
+
+    private void setText(Calendar cal, int options1, int options2, int options3) {
+        String str1 =weeks.get(options1);
+        String str2 =hours.get(options2);
+        String str3 =minutes.get(options3);
+                /*Pattern p = Pattern.compile("\\d+");
+                Matcher m = p.matcher(str);
+                m.find();
+                String newStr = m.group();*/
+        String[] nums = new String[2];
+        if (str1.equals(getString(R.string.today))) {
+            nums[0] = String.format("%02d", cal.get(Calendar.MONTH) + 1);
+            nums[1] = String.format("%02d", cal.get(Calendar.DAY_OF_MONTH));
+        } else {
+            nums = str1.split("\\D+");
+        }
+        edtTime.setText(cal.get(Calendar.YEAR)+"-"+String.format("%02d", Integer.parseInt(nums[0])) + "-"
+                + String.format("%02d", Integer.parseInt(nums[1])) + getString(R.string.blank_space) + str2 + ":" + str3);
     }
 
     @Override
@@ -95,6 +169,7 @@ public class InvestigationActivity extends BaseActivity {
                     break;
 
                 case R.id.edtTime:
+                    pvCustomTime.show();
                     break;
             }
         }
