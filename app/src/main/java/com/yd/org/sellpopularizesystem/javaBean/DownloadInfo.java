64d44cf46f3ca75b543528ea15c88bf57466b4dd
eougene <1365423747@qq.com>
2017-06-02 13:24:56 +0800

    变动

---
+++ app/src/main/java/com/yd/org/sellpopularizesystem/javaBean/DownloadInfo.java
@@ -0,0 +1,69 @@
+package com.yd.org.sellpopularizesystem.javaBean;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by hejin on 2017/6/1.
+ */
+
+public class DownloadInfo implements Parcelable {
+    private int progress;
+    private long currentFileSize;
+    private long totalFileSize;
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public void setProgress(int progress) {
+        this.progress = progress;
+    }
+
+    public long getCurrentFileSize() {
+        return currentFileSize;
+    }
+
+    public void setCurrentFileSize(long currentFileSize) {
+        this.currentFileSize = currentFileSize;
+    }
+
+    public long getTotalFileSize() {
+        return totalFileSize;
+    }
+
+    public void setTotalFileSize(long totalFileSize) {
+        this.totalFileSize = totalFileSize;
+    }
+    public DownloadInfo() {
+    }
+    protected DownloadInfo(Parcel in) {
+        this.progress = in.readInt();
+        this.currentFileSize = in.readLong();
+        this.totalFileSize = in.readLong();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.progress);
+        dest.writeLong(this.currentFileSize);
+        dest.writeLong(this.totalFileSize);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<DownloadInfo> CREATOR = new Creator<DownloadInfo>() {
+        @Override
+        public DownloadInfo createFromParcel(Parcel in) {
+            return new DownloadInfo(in);
+        }
+
+        @Override
+        public DownloadInfo[] newArray(int size) {
+            return new DownloadInfo[size];
+        }
+    };
+}
