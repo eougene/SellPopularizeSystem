0f12522429367f474982643195b80ce30f5b9b4e
eougene <1365423747@qq.com>
2017-06-02 13:22:35 +0800

    变动

--- app/src/main/java/com/yd/org/sellpopularizesystem/activity/PDFActivity.java
+++ app/src/main/java/com/yd/org/sellpopularizesystem/activity/PDFActivity.java
@@ -1,10 +1,13 @@
 package com.yd.org.sellpopularizesystem.activity;
 
 import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
 import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.shockwave.pdfium.PdfDocument;
 import com.yd.org.sellpopularizesystem.R;
 import com.yd.org.sellpopularizesystem.application.Contants;
 import com.yd.org.sellpopularizesystem.javaBean.FileContent;
@@ -12,14 +15,17 @@ import com.yd.org.sellpopularizesystem.utils.PDFUtils;
 import com.yd.org.sellpopularizesystem.utils.ToasShow;
 
 import java.io.File;
+import java.util.List;
 
 import pdfinterface.OnFileListener;
+
+import static com.android.volley.VolleyLog.TAG;
 
 public class PDFActivity extends BaseActivity implements OnPageChangeListener
         , OnLoadCompleteListener {
     private PDFView pdfView;
     private FileContent fileContent;
-
+    private String sale_advice_url, orderId;
 
     @Override
     protected int setContentView() {
@@ -29,12 +35,19 @@ public class PDFActivity extends BaseActivity implements OnPageChangeListener
     @Override
     public void initView() {
         hideRightImagview();
-
-        fileContent = (FileContent) getIntent().getSerializableExtra("pdf");
-        setTitle(fileContent.getDetail_name());
-
         pdfView = (PDFView) findViewById(R.id.pdfView);
-        displayFromFile1(Contants.DOMAIN + "/" + fileContent.getUrl(), "pdfa_"+ fileContent.getProduct_id() + ".pdf");
+        Bundle bundle = getIntent().getExtras();
+        orderId = bundle.getString("orderId");
+        sale_advice_url = bundle.getString("sale_advice_url");
+        if (orderId==null || sale_advice_url==null){
+            fileContent = (FileContent) getIntent().getSerializableExtra("pdf");
+            setTitle(fileContent.getDetail_name());
+            displayFromFile1(Contants.DOMAIN + "/" + fileContent.getUrl(), "pdfa_"+ fileContent.getProduct_id() + ".pdf");
+        }else {
+            setTitle(getString(R.string.sale_notice));
+            displayFromFile1(sale_advice_url, "srna_" + orderId + ".pdf");
+        }
+
 
 
     }
@@ -82,6 +95,18 @@ public class PDFActivity extends BaseActivity implements OnPageChangeListener
     public void onPageChanged(int page, int pageCount) {
         ToasShow.showToastBottom(PDFActivity.this, page +
                 "/" + pageCount);
+        printBookmarksTree(pdfView.getTableOfContents(), "-");
+    }
+
+    public void printBookmarksTree(List<PdfDocument.Bookmark> tree, String sep) {
+        for (PdfDocument.Bookmark b : tree) {
+
+            Log.e(TAG, String.format("%s %s, p %d", sep, b.getTitle(), b.getPageIdx()));
+
+            if (b.hasChildren()) {
+                printBookmarksTree(b.getChildren(), sep + "-");
+            }
+        }
     }
 
 
