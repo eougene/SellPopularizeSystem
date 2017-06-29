68eb6db531972f106d6ea1591431da7bcb5e9cb6
白鹏 <1191884034@qq.com>
2017-06-29 17:52:18 +0800

    ff

--- app/src/main/java/com/yd/org/sellpopularizesystem/getui/IntentService.java
+++ app/src/main/java/com/yd/org/sellpopularizesystem/getui/IntentService.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Message;
 import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.igexin.sdk.GTIntentService;
@@ -15,7 +16,9 @@ import com.igexin.sdk.message.GTCmdMessage;
 import com.igexin.sdk.message.GTTransmitMessage;
 import com.yd.org.sellpopularizesystem.R;
 import com.yd.org.sellpopularizesystem.activity.HomeActiviyt;
+import com.yd.org.sellpopularizesystem.activity.LoginActivity;
 import com.yd.org.sellpopularizesystem.application.BaseApplication;
+import com.yd.org.sellpopularizesystem.utils.SharedPreferencesHelps;
 
 /**
  * 继承 GTIntentService 接收来自个推的消息, 所有消息在线程中回调, 如果注册了该服务, 则务必要在 AndroidManifest中声明, 否则无法接受消息<br>
@@ -103,7 +106,17 @@ public class IntentService extends GTIntentService {
         builder.setDefaults(NotificationCompat.DEFAULT_ALL);//设置通知的方式，震动、LED灯、音乐等
         builder.setAutoCancel(true);//点击通知后，状态栏自动删除通知
         builder.setSmallIcon(R.mipmap.notification);//设置小图标
-        builder.setContentIntent(PendingIntent.getActivity(this, 0x102, new Intent(this, HomeActiviyt.class), 0));//设置点击通知后将要启动的程序组件对应的PendingIntent
+
+
+        //未登录
+        if (SharedPreferencesHelps.getUserID().equals("null") || TextUtils.isEmpty(SharedPreferencesHelps.getUserID()) || SharedPreferencesHelps.getUserID() == null) {
+            builder.setContentIntent(PendingIntent.getActivity(this, 0x102, new Intent(this, LoginActivity.class), 0));//设置点击通知后将要启动的程序组件对应的PendingIntent
+        } else {
+            //已登录
+            builder.setContentIntent(PendingIntent.getActivity(this, 0x102, new Intent(this, HomeActiviyt.class), 0));//设置点击通知后将要启动的程序组件对应的PendingIntent
+        }
+
+
         Notification notification = builder.build();
         //发送通知
         notificationManager.notify(messageNotificationID, notification);
