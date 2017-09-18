e635856d2d9c4323c9693746367fededcb97fd65
eougene <1365423747@qq.com>
2017-09-18 14:39:13 +0800

    提交

--- app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -14,12 +14,12 @@ public final class R {
         public static final int stackFromEnd = 0x7f01012b;
     }
     public static final class dimen {
-        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f0a008c;
-        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f0a008d;
-        public static final int item_touch_helper_swipe_escape_velocity = 0x7f0a008e;
+        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f0b008c;
+        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f0b008d;
+        public static final int item_touch_helper_swipe_escape_velocity = 0x7f0b008e;
     }
     public static final class id {
-        public static final int item_touch_helper_previous_elevation = 0x7f100005;
+        public static final int item_touch_helper_previous_elevation = 0x7f10000e;
     }
     public static final class styleable {
         public static final int[] RecyclerView = { 0x010100c4, 0x010100f1, 0x7f010128, 0x7f010129, 0x7f01012a, 0x7f01012b };
